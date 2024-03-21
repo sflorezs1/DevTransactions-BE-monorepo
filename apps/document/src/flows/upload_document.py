1e509435734e14e5fedfa86aa2fa1cfd5e94f351
David Tibaduiza <davidet9412@gmail.com>
2024-03-21 01:52:47 -0500

    get document id and get all documents working

--- apps/document/src/flows/upload_document.py
+++ apps/document/src/flows/upload_document.py
@@ -8,7 +8,7 @@ from auth.api_dependency import ContextAuth
 from cryptography.fernet import Fernet
 from faststream import Depends, FastStream
 from faststream.rabbit import RabbitBroker
-from sqlalchemy import select
+from sqlalchemy import select, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 from ..config import SQLALCHEMY_DATABASE_URI
 from queues.queues import  OperatorInfo, Queues, TransferFilePayload, TransferUserPayload, UploadDocument
@@ -48,23 +48,33 @@ def upload_document_flow(app: FastStream, broker: RabbitBroker):
 def get_all_document_flow(app: FastStream, broker: RabbitBroker):
     @broker.subscriber(Queues.GET_ALL_DOCUMENTS.value)
     async def handle_get_all_documents(auth: ContextAuth, session: AsyncSession = Depends(inject_session)):
-        documents = await session.execute(select(Document))
+        documents = await session.execute(select(Document).where(Document.email==auth.email))
         documents = documents.scalars().all()
-        await broker.publish(documents, Queues.GET_ALL_DOCUMENTS.value) 
-
+        
+        logger.info(f"documents: {documents}")
+        return {"documents":[{ "id": x.id, "filename": x.filename } for x in documents]}
+    
+    
     
 def get_document_by_id_flow(app: FastStream, broker: RabbitBroker):
     @broker.subscriber(Queues.GET_DOCUMENT_BY_ID.value)
     async def handle_get_document_by_id( document_id: str,auth: ContextAuth,session: AsyncSession = Depends(inject_session)):       
-        document = await session.execute(select(Document).where(Document.id == document_id))
+
+        document = await session.execute(select(Document).where(Document.id == document_id, and_(Document.email==auth.email)))
         document = document.scalar_one_or_none()
+        adapter = GCPStorageAdapter()
+        url=adapter.generate_presigned_get_url(document.gcs_path,expiration=datetime.timedelta(minutes=10))
+
         if document:
-            return {"document": document.__dict__}
+            return {"document":{
+                "filename":document.filename,
+                "id":document.id,
+                "url":url
+            }}
         else:
             return None
         
-# y lo autenticacion mirar el de subir archivo
-        
+
 def operator_transfer_add_files(app: FastStream, broker: RabbitBroker):
     @broker.subscriber(Queues.ADD_USER_TRANSFER_DOCS_INFO.value)
     async def handle_user_transfer_docs_info(transfer_payload: TransferUserPayload, operator_info: OperatorInfo, session: AsyncSession = Depends(inject_session)):
