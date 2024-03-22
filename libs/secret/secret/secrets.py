55542eef55643d497ac0dd1962e6d59e10cf2c4f
Simón Flórez Silva <53716139+sflorezs1@users.noreply.github.com>
2024-03-22 18:01:55 -0500

    Update secrets.py

--- libs/secret/secret/secrets.py
+++ libs/secret/secret/secrets.py
@@ -2,7 +2,7 @@ import os
 from google.cloud import secretmanager
 from .config import PROJECT_ID
 
-def access_secret_version(secret_id: str, default_value: str = None, version_id: str = 'latest'):
+def access_secret_version(secret_id: str, default_value: str = None, version_id: str = 'latest':
     env = os.getenv('ENV', 'dev')
     if env != 'prod' and default_value is not None:
         return default_value
@@ -10,4 +10,5 @@ def access_secret_version(secret_id: str, default_value: str = None, version_id:
     client = secretmanager.SecretManagerServiceClient()
     name = client.secret_version_path(project=PROJECT_ID, secret=secret_id, secret_version=version_id)
     response = client.access_secret_version(name=name)
+    raise Error('Break for test')
     return response.payload.data.decode('UTF-8').replace('\n', '')
